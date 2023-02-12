356b934715235e4408beb5549e02ca5df0c8a2e2
XevenLord <chuazonghong@gmail.com>
2023-02-12 17:23:02 +0800

    E-commerce Demo: Add ProductBox to display list of products

--- src/views/Product/Product.vue
+++ src/views/Product/Product.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="container">
     <div class="row">
-        <h4>Our Products</h4>
+      <h4>Our Products</h4>
     </div>
     <div class="row">
-        
+      <div v-for="product of products" :key="product" class="col-md-6 col-xl-4 col-12 pt-3 d-flex">
+        <ProductBox :product="product"></ProductBox>
+      </div>
     </div>
   </div>
 </template>
+<script>
+import ProductBox from "../../components/ProductBox";
+export default {
+  components: { ProductBox },
+  props: ["products"],
+};
+</script>
